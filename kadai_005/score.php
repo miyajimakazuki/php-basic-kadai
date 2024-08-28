0ff4c800afdf441292d8410bb82941769e60c9a2
miyajimakazuki <miyajima@csplan.jp>
2024-08-28 10:48:35 +0900

    kadai_005.php提出

---
+++ kadai_005/score.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>kadai_005</title>
+</head>
+<body>
+    <?php
+    $score = [80, 60, 55, 40, 100, 25, 80, 95, 30, 60];
+    echo ($score[0]+$score[1]+$score[2]+$score[3]+$score[4]+$score[5]
+    +$score[6]+$score[7]+$score[8]+$score[9])/10;
+    ?>
+</body>
+</html>

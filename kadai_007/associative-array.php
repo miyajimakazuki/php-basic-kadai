2efdbd240df5c1bc81ec61bafd1ac29e56cadb04
miyajimakazuki <miyajima@csplan.jp>
2024-08-28 11:45:48 +0900

    kadai_007.php提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>kadai_007</title>
+</head>
+<body>
+    <p>
+        <?php
+            $yasai = [
+                'name' => 'onion',
+                'price' => 200,
+                'weight' => 160,
+            ];
+            print_r($yasai);
+
+        ?>
+    </p>
+</body>
+</html>
